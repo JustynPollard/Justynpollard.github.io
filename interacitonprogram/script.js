e6c485201310060987d67e4843ef42134d181ea6
Justyn Pollard <justynpollard12@hotmail.com>
2018-02-08 14:32:08 -0600

    added device shaken

--- interacitonprogram/script.js
+++ interacitonprogram/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Interaciton program
 // Justyn Pollard
 // Feb 7, 2018
@@ -16,43 +15,18 @@ function draw() {
       fill(255, 150, 150);
       rect(mouseX, mouseY, 150, 150);
     }
-  }
-  if (keyIsPressed) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ellipse(mouseX, mouseY, random(50, 300), random(50, 300));
-  }
-  function deviceShaken() {
-    rect(255, 255, 255, 255)
-  }
+    if (mouseButton === RIGHT) {
 
-}
-=======
-// Interaciton program
-// Justyn Pollard
-// Feb 7, 2018
-
-// global variables //
-
-// Functions //
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background('white');
-  if (mouseIsPressed) {
-    if (mouseButton === LEFT) {
-      fill(255, 150, 150);
-      rect(mouseX, mouseY, 150, 150);
     }
   }
   if (keyIsPressed) {
     fill(random(0, 255), random(0, 255), random(0, 255));
     ellipse(mouseX, mouseY, random(50, 300), random(50, 300));
   }
-  function deviceShaken() {
-    rect(255, 255, 255, 255)
-  }
-
 }
->>>>>>> 9599bc4863e8ea7dd66bafd007afb992053cf1c0
+
+function deviceShaken() {
+  fill('black')
+  textSize(32);
+  text('Sup', width / 2, height / 2);
+}
